d50a97e9c42526b613b003ba83c4e04894e4afa3
Jorge Cordero <jorgecorderozx@gmail.com>
2026-07-09 21:09:18 -0400

    Function to create task on array added Function to create li in DOM added

--- index.js
+++ index.js
@@ -9,71 +9,42 @@ function addClass(tag, className){
     tag.classList.add(className);
 }
 
-function createTask(taskName){
-    let li = document.createElement("li")
-    let checkButton = document.createElement("button");
-    let task = document.createElement("span");
-    task.textContent = taskName;
-    let crossButton = document.createElement("button");
-    li.dataset.id = saveTask(taskName);
-    addClass(li, "flex");
-    addClass(checkButton, "check");
-    addClass(crossButton, "cross");
-
-    li.appendChild(checkButton);
-    li.appendChild(task)
-    li.appendChild(crossButton);
-
-    checkButton.addEventListener("click", ()=>{
-        li.classList.toggle("completed")
-        const id = Number(li.dataset.id);
-        const taskObject = tasks.find(t => t.id === id);
-        if(taskObject){
-            taskObject.completed = li.classList.contains("completed");
-        }
-        updateItemsLeft()
-    })
-
-    crossButton.addEventListener("click", () =>{
-        const id = Number(li.dataset.id);
-        tasks = tasks.filter(t => t.id !== id);
-        li.remove()
-        updateItemsLeft()
-    })
-
-    return li;
-}
-
-function saveTask(taskName){
-    idCounter++;
+function createTask(taskDescription){
+    idCounter++
     let task = {
         id: idCounter,
-        text: taskName,
+        description: taskDescription,
         completed: false
     }
     tasks.push(task)
-    return task.id;
 }
 
-function renderTask(){
-    if(taskInput.value === "") {
-        return;
-    } 
-    else{
-        tasksContainer.appendChild(createTask(taskInput.value));
-        taskInput.value = "";
-        updateItemsLeft();
-    }
-}
+
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    renderTask();
-
+    if(taskInput.value.trim() === "") return
+    else{
+        createTask(taskInput.value);
+    }
 })
 
-function updateItemsLeft(){
-    const remainingItems = document.querySelector(".remaining-items");
-    const remainingCount = tasks.filter(t => t.completed !== true).length
-    remainingCount === 1 ? remainingItems.textContent = `${remainingCount} item left` :  remainingItems.textContent = `${remainingCount} items left`;
+function renderTask(taskDescription){
+    const taskItem = document.createElement("li");
+    const checkButton = document.createElement("button");
+    const taskText = document.createElement("span");
+    const crossButton = document.createElement("button");
+
+    addClass(taskItem, "flex");
+    addClass(checkButton, "check");
+    addClass(crossButton, "cross");
+
+    taskItem.appendChild(checkButton);
+    taskItem.appendChild(taskText);
+    taskItem.appendChild(crossButton);
+
+    taskText.textContent = taskDescription;
+
+    tasksContainer.appendChild(taskItem)
+
 }
